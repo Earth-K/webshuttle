7d2a047e6041cd67e00b6d1639875e40b8b3b8a6
earth-k <k94earth@naver.com>
2022-01-29 18:39:48 +0900

    Refactoring - method extraction, split class
    What>
    Add EventListenerInjector.py

---
+++ EventListenerInjector.py
@@ -0,0 +1,75 @@
+class EventListenerInjector:
+
+    def __init__(self, webCrawler):
+        self._webCrawler = webCrawler
+
+    def addMouseOverLeave(self):
+        script = ''' 
+        let childNodes = document.getElementsByTagName('body')[0].childNodes;
+        const func = (c) => {
+          if(c==undefined) return;
+          for(let i = 0 ; i<c.length; i++) {
+            c[i].addEventListener("mouseover", function(event) {
+                event.stopPropagation();
+                event.target.style.border = "3px solid rgba(217,217,243,90)";
+            });
+            c[i].addEventListener("mouseleave", function(event) {
+                event.stopPropagation();
+                event.target.style.border = "";
+            });
+            func(c[i].childNodes);
+          }
+        }
+        func(childNodes);
+        '''
+        self._webCrawler.executeJs(script)
+
+    def addTooltip(self):
+        script = ''' 
+        let wsTooltip = document.createElement('div');
+        wsTooltip.id = "ws-tooltip";
+        wsTooltip.style.width = "100px";
+        wsTooltip.style.height = "45px";
+        wsTooltip.style.backgroundColor = "tomato";
+        wsTooltip.style.display = "flex";
+        wsTooltip.style.justifyContent = "center";
+        wsTooltip.style.alignItems = "center";
+
+        let wsBtn = document.createElement('button');
+        wsBtn.id = "ws-select-btn";
+        wsBtn.innerText = "선택";
+        wsBtn.style.width = "50px";
+        wsBtn.style.height = "30px"
+
+        wsTooltip.appendChild(wsBtn);
+
+        document.body.appendChild(wsTooltip);
+        '''
+        self._webCrawler.executeJs(script)
+
+    def addMouseDownRight(self):
+        script = ''' 
+        window.oncontextmenu = function (event) {   event.preventDefault() }
+        let childNodes = document.getElementsByTagName('body')[0].childNodes;
+        const func = (c) => {
+          if(c==undefined) return;
+          for(let i = 0 ; i<c.length; i++) {
+            c[i].addEventListener("mousedown", function(event) {
+                if(event.which != 3) return;
+                let posTop = window.scrollY + event.clientY+3;
+                let posLeft = event.clientX+3;
+                let wsTooltip = document.getElementById("ws-tooltip"); 
+                wsTooltip.style.position = "absolute";
+                wsTooltip.style.top = posTop.toString()+"px";
+                wsTooltip.style.left = posLeft.toString()+"px";
+                wsTooltip.style.display = "flex";
+                wsTooltip.style.justifyContent = "center";
+                wsTooltip.style.alignItems = "center";
+                event.stopPropagation();
+            });
+            func(c[i].childNodes);
+          }
+        }
+        func(childNodes);
+        '''
+        self._webCrawler.executeJs(script)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+
+from EventListenerInjector import EventListenerInjector
 from WebCrawler import WebCrawler
 from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QPushButton, QVBoxLayout, QTextEdit, QLineEdit
 
@@ -40,80 +42,13 @@ class MyApp(QWidget):
 
     def _openBrowser(self):
         self._webCrawler = WebCrawler(self._lineText.text())
-        self._addMouseOverLeave()
-        self._addTooltip()
-        self._addMouseDownRight()
+        self._initEventListener(self._webCrawler)
 
-    def _addMouseOverLeave(self):
-        script = ''' 
-        let childNodes = document.getElementsByTagName('body')[0].childNodes;
-        const func = (c) => {
-          if(c==undefined) return;
-          for(let i = 0 ; i<c.length; i++) {
-            c[i].addEventListener("mouseover", function(event) {
-                event.stopPropagation();
-                event.target.style.border = "3px solid rgba(217,217,243,90)";
-            });
-            c[i].addEventListener("mouseleave", function(event) {
-                event.stopPropagation();
-                event.target.style.border = "";
-            });
-            func(c[i].childNodes);
-          }
-        }
-        func(childNodes);
-        '''
-        self._webCrawler.executeJs(script)
-
-    def _addTooltip(self):
-        script = ''' 
-        let wsTooltip = document.createElement('div');
-        wsTooltip.id = "ws-tooltip";
-        wsTooltip.style.width = "100px";
-        wsTooltip.style.height = "45px";
-        wsTooltip.style.backgroundColor = "tomato";
-        wsTooltip.style.display = "flex";
-        wsTooltip.style.justifyContent = "center";
-        wsTooltip.style.alignItems = "center";
-        
-        let wsBtn = document.createElement('button');
-        wsBtn.id = "ws-select-btn";
-        wsBtn.innerText = "선택";
-        wsBtn.style.width = "50px";
-        wsBtn.style.height = "30px"
-        
-        wsTooltip.appendChild(wsBtn);
-        
-        document.body.appendChild(wsTooltip);
-        '''
-        self._webCrawler.executeJs(script)
-
-    def _addMouseDownRight(self):
-        script = ''' 
-        window.oncontextmenu = function (event) {   event.preventDefault() }
-        let childNodes = document.getElementsByTagName('body')[0].childNodes;
-        const func = (c) => {
-          if(c==undefined) return;
-          for(let i = 0 ; i<c.length; i++) {
-            c[i].addEventListener("mousedown", function(event) {
-                if(event.which != 3) return;
-                let posTop = window.scrollY + event.clientY+3;
-                let posLeft = event.clientX+3;
-                let wsTooltip = document.getElementById("ws-tooltip"); 
-                wsTooltip.style.position = "absolute";
-                wsTooltip.style.top = posTop.toString()+"px";
-                wsTooltip.style.left = posLeft.toString()+"px";
-                wsTooltip.style.display = "flex";
-                wsTooltip.style.justifyContent = "center";
-                wsTooltip.style.alignItems = "center";
-                event.stopPropagation();
-            });
-            func(c[i].childNodes);
-          }
-        }
-        func(childNodes);
-        '''
-        self._webCrawler.executeJs(script)
+    def _initEventListener(self, webCrawler):
+        injector = EventListenerInjector(webCrawler)
+        injector.addTooltip()
+        injector.addMouseOverLeave()
+        injector.addMouseDownRight()
 
 
 if __name__ == '__main__':
