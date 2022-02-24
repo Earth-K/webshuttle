b3c12ffc4192f56494cfae0a0fa5cb9c60f3375a
earth-k <k94earth@naver.com>
2022-02-25 01:57:19 +0900

    Make only necessary button enable state. and implement the stop button.

--- widgets/ShuttlesWidget.py
+++ widgets/ShuttlesWidget.py
@@ -1,7 +1,7 @@
 import threading
 import time
 
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout, QLineEdit, QPushButton
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout, QLineEdit, QPushButton, QMessageBox
 from selenium import webdriver
 
 from WebCrawler import WebCrawler
@@ -37,7 +37,6 @@ class ShuttlesWidget(QWidget):
         url_lineedit.setReadOnly(True)
         period_lineedit = QLineEdit()
         period_lineedit.setText(period)
-        period_lineedit.setReadOnly(True)
         target_classes_lineedit = QLineEdit()
         target_classes_lineedit.setText(target_classes)
         target_classes_lineedit.setReadOnly(True)
@@ -51,9 +50,17 @@ class ShuttlesWidget(QWidget):
         hbox_layout_shuttle.addWidget(QLabel('target classes : '))
         hbox_layout_shuttle.addWidget(target_classes_lineedit)
         start_btn = QPushButton('Start')
-        start_btn.clicked.connect(lambda: self._start(url_lineedit, period_lineedit, target_classes_lineedit, log_edittext))
+        stop_btn = QPushButton('Stop')
+
+        start_btn.clicked.connect(
+            lambda: self._start(url_lineedit, period_lineedit, target_classes_lineedit, log_edittext, start_btn,
+                                stop_btn))
         hbox_layout_shuttle.addWidget(start_btn)
-        hbox_layout_shuttle.addWidget(QPushButton('Stop'))
+
+        stop_btn.setDisabled(True)
+        stop_btn.clicked.connect(lambda: self._stop(period_lineedit, start_btn, stop_btn))
+        hbox_layout_shuttle.addWidget(stop_btn)
+
         hbox_layout_memo = QHBoxLayout()
         hbox_layout_memo.addWidget(QLabel('description : '))
         description = QLineEdit()
@@ -63,14 +70,28 @@ class ShuttlesWidget(QWidget):
         vbox_wrap_layout.addLayout(hbox_layout_shuttle)
         vbox_wrap_layout.addLayout(hbox_layout_memo)
         self._vbox_layout.addLayout(vbox_wrap_layout)
+        self._vbox_layout.addStretch(4)
 
-    def _start(self, url, period, target_classes, edittext):
+    def _stop(self, period, start_btn, stop_btn):
+        period.setReadOnly(False)
+        stop_btn.setDisabled(True)
+        start_btn.setDisabled(False)
+
+    def _start(self, url, period, target_classes, log_edittext, start_btn, stop_btn):
+        if not period.text().isdigit():
+            QMessageBox.information(self, 'Please input a number',
+                                    "You have to input a number at field of 'check period'.",
+                                    QMessageBox.Yes, QMessageBox.NoButton)
+            return
+        period.setReadOnly(True)
+        start_btn.setDisabled(True)
+        stop_btn.setDisabled(False)
         thread = threading.Thread(target=self._check_content,
-                                  args=(url, period, target_classes, edittext))
+                                  args=(url, period, target_classes, log_edittext, start_btn))
         thread.daemon = True
         thread.start()
 
-    def _check_content(self, url, period, target_classes, edittext):
+    def _check_content(self, url, period, target_classes, log_edittext, start_btn: QPushButton):
         while True:
             options = webdriver.ChromeOptions()
             options.add_argument('headless')
@@ -81,6 +102,9 @@ class ShuttlesWidget(QWidget):
             time.sleep(1)
             elements = tmp_web_crawler.get_elements_by_classnames(target_classes.text())
             for e in elements:
-                edittext.append(e.text)
+                log_edittext.append(e.text)
             tmp_web_crawler.close_driver()
+
+            if start_btn.isEnabled() is True:
+                break
             time.sleep(int(period.text()))
