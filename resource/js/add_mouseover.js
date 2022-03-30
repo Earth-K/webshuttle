0f78967dbee5c7f3714bb3aae29348d480517079
earth-k <k94earth@naver.com>
2022-03-31 00:38:22 +0900

    Rename javascript variables.

--- resource/js/add_mouseover.js
+++ resource/js/add_mouseover.js
@@ -1,12 +1,12 @@
 let childNodes = document.getElementsByTagName('body')[0].childNodes;
-const func = (c) => {
-    if (c === undefined) return;
-    for (let i = 0; i < c.length; i++) {
-        c[i].addEventListener("mouseover", function (event) {
+const addMouseoverEventAll = (children) => {
+    if (children === undefined) return;
+    for (let i = 0; i < children.length; i++) {
+        children[i].addEventListener("mouseover", function (event) {
             event.stopPropagation();
             event.target.style.backgroundColor = "rgba(217,217,243,0.5)";
         });
-        func(c[i].childNodes);
+        addMouseoverEventAll(children[i].childNodes);
     }
 }
-func(childNodes);
+addMouseoverEventAll(childNodes);
--- resource/js/add_originBackgroundColor.js
+++ resource/js/add_originBackgroundColor.js
@@ -1,9 +1,9 @@
 let childNodes = document.getElementsByTagName('body')[0].childNodes;
-const func = (c) => {
-    if (c === undefined) return;
-    for (let i = 0; i < c.length; i++) {
-        c[i].setAttribute("data-originBackgroundColor", c[i].style.backgroundColor);
-        func(c[i].childNodes);
+const addOriginBackgroundAttributeAll = (children) => {
+    if (children === undefined) return;
+    for (let i = 0; i < children.length; i++) {
+        children[i].setAttribute("data-originBackgroundColor", children[i].style.backgroundColor);
+        addOriginBackgroundAttributeAll(children[i].childNodes);
     }
 }
-func(childNodes);
+addOriginBackgroundAttributeAll(childNodes);
