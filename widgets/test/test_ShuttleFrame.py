9d182ee646e060ddfd67ea7041b7dc3af414e3e8
earth-k <k94earth@naver.com>
2022-05-28 00:26:19 +0900

    Add UI test for layout of settings dialog.

--- widgets/test/test_ShuttleFrame.py
+++ widgets/test/test_ShuttleFrame.py
@@ -1,7 +1,7 @@
 import sys
 
 import pytest
-from PyQt5.QtWidgets import QLineEdit, QTextEdit, QApplication, QSpinBox, QWidget
+from PyQt5.QtWidgets import QLineEdit, QTextEdit, QApplication, QSpinBox, QWidget, QVBoxLayout
 
 from domain.ShuttleWidgetGroup import ShuttleWidgetGroup
 from widgets.ShuttleFrame import ShuttleFrame
@@ -43,7 +43,7 @@ def test_settings_are_applied_when_clicked_ok(qapp):
 def test_apply_draft_shuttle(qapp):
     parent = QWidget()
     spinbox = QSpinBox()
-    spinbox.setMaximum(38000)
+    spinbox.setMaximum(86000)
     shuttleFrame = ShuttleFrame(shuttles={}, shuttle_seq=0, chrome_service=None,
                                 shuttleWidgetGroup=ShuttleWidgetGroup(url_widget=QLineEdit(),
                                                                       update_list_widget=QTextEdit(),
@@ -65,3 +65,32 @@ def test_apply_draft_shuttle(qapp):
     assert shuttleFrame.shuttleWidgets.target_classes_widget.text() == shuttleFrame.draft_shuttleWidgets.target_classes.text()
     assert shuttleFrame.shuttleWidgets.period_widget.value() == shuttleFrame.draft_shuttleWidgets.period.value()
     assert shuttleFrame.frame_name.text() == "name"
+
+
+def test_UI_of_showSettings_QDialog(qapp):
+    parent = QWidget()
+    spinbox = QSpinBox()
+    spinbox.setMaximum(86000)
+    shuttleFrame = ShuttleFrame(shuttles={}, shuttle_seq=0, chrome_service=None,
+                                shuttleWidgetGroup=ShuttleWidgetGroup(url_widget=QLineEdit(),
+                                                                      update_list_widget=QTextEdit(),
+                                                                      shuttle_name_widget=QLineEdit(),
+                                                                      period_widget=spinbox,
+                                                                      target_classes_widget=QLineEdit(),
+                                                                      parent=None
+                                                                      ),
+                                parent=parent, time=None)
+
+    shuttleFrame.setSettingsLayout()
+
+    layout: QVBoxLayout = shuttleFrame.vBoxLayout
+    assert layout.count() == 5
+    assert layout.itemAt(0).layout().itemAt(0).widget().text() == "셔틀 이름 : "
+    assert type(layout.itemAt(0).layout().itemAt(1).widget()) == QLineEdit
+    assert layout.itemAt(1).layout().itemAt(0).widget().text() == "URL : "
+    assert type(layout.itemAt(1).layout().itemAt(1).widget()) == QLineEdit
+    assert layout.itemAt(2).layout().itemAt(0).widget().text() == "반복 주기(초) : "
+    assert type(layout.itemAt(2).layout().itemAt(1).widget()) == QSpinBox
+    assert layout.itemAt(2).layout().itemAt(1).widget().maximum() == 86000
+    assert layout.itemAt(3).layout().itemAt(0).widget().text() == "타깃 클래스 : "
+    assert type(layout.itemAt(3).layout().itemAt(1).widget()) == QLineEdit
