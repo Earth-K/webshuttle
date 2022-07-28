bf7182e684927b7d59627681cd85c9ed8026ea98
GreanNeuron <goodev@kakao.com>
2022-07-29 02:58:05 +0900

    (refactor): UpdatedTextParser 메서드 추출

--- webshuttle/domain/UpdatedTextParser.py
+++ webshuttle/domain/UpdatedTextParser.py
@@ -16,25 +16,30 @@ class UpdatedTextParser:
         for e in self.elements:
             text: str = e.text
             if len(text) > 0:
-                if text.find(self.filtering_keyword) == -1:
+                if not self._has_filtering_keyword(text):
                     continue
                 self.text_list.append(text)
-                # 한 번에 보이는 정보의 양을 늘리기 위해 줄 바꿈 문자를 | 로 변경함
-                self.collected_text += text.replace("\n", " | ") + "\n"
+                self._collect_text(text)
 
     def _collect_updated_text(self):
-        new_text_list = self._text_list()
+        new_text_list = self._new_text_list()
         for new_text in new_text_list:
             if new_text not in self.text_list:
                 if len(new_text) > 0:
-                    # 한 번에 보이는 정보의 양을 늘리기 위해 줄 바꿈 문자를 | 로 변경함
-                    self.collected_text += new_text.replace("\n", " | ") + "\n"
+                    self._collect_text(new_text)
 
-    def _text_list(self):
+    def _collect_text(self, text):
+        # 한 번에 보이는 정보의 양을 늘리기 위해 줄 바꿈 문자를 | 로 변경함
+        self.collected_text += text.replace("\n", " | ") + "\n"
+
+    def _new_text_list(self):
         result = []
         for e in self.elements:
             text: str = e.text
-            if text.find(self.filtering_keyword) == -1:
+            if not self._has_filtering_keyword(text):
                 continue
             result.append(text)
         return result
+
+    def _has_filtering_keyword(self, text):
+        return text.find(self.filtering_keyword) > -1
